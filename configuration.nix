da970c9aed76f7ee134165ea8dbae7d7146875b9
rdusak <87875782+rdusak@users.noreply.github.com>
2022-01-21 02:46:22 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -39,4 +39,58 @@
    services.xserver.displayManager.sddm.enable = true;
    services.xserver.desktopmanager.plasma5.enable = true;
    
+   # Configure keymap in X11
+   services.xserver.layout = "hr";
+   services.xserver.xkbOptions = "eurosign:e";
+   
+   # Enable CUPS to print documents
+   # services.printing.enable = true;
+   
+   # Enable sound
+   sound.enable = true;
+   hardware.pulseaudio.enable = true;
+   
+   # Enable touchpad support
+   # services.xserver.libinput.enable = true;
+   
+   # Define a user account. 
+   users.users.robert = {
+      isNormalUser = true;
+      initialPassword = "pass"; # Don't forget to change this after inital login
+      extraGroups = [ "wheel" ]; # Enable 'sudo' for the user
+   };
+   
+   # List packages installed in system profile. To search, run:
+   # $ nix search wget
+   environment.systemPackges = with pkgs; [
+      wget vim nano
+      firefox
+   ];
+   
+   # Some programs need SUID wrappers, can be configured further or are
+   # started in user sessions.
+   # programs.mtr.enable = true;
+   # programs.gnupg.agent = {
+   #    enable = true;
+   #    enableSSHSupport = true;
+   # };
+   
+   # List service that you want to enable:
+   
+   # Enable the OpenSSH daemon:
+   # services.openssh.enable = true;
+   
+   # Open ports in the firewall:
+   # networking.firewall.allowedTCPPorts = [ ... ];
+   # networking.firewall.allowedUDPPorts = [ ... ];
+   # Or disable the firewall altogether:
+   # networking.firewall.enable = false;
+   
+   # This value determins the NixOS release from which the default
+   # settings for stateful data, like file locations and database versions
+   # on your system were taken. It's perfectly fine and recommended to leave
+   # this value at the release version of the first install of this system.
+   # Before changing this value read the documentation for this option:
+   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html)
+   system.stateVersion = "21.11"; # Did you read the comment?
 }
